0a42ff37ec91490397c20ba87e083b02d737b494
이준용 <iiiuooyoo@gmail.com>
2023-08-28 17:49:13 +0900

    feat : matter-js 예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,35 @@
+import { useEffect } from "react";
+import { Engine, Render, Bodies, Composite, Runner } from "matter-js";
+
 function App() {
+    useEffect(() => {
+        // create an engine
+        var engine = Engine.create();
+
+        // create a renderer
+        var render = Render.create({
+            element: document.body,
+            engine: engine,
+        });
+
+        // create two boxes and a ground
+        var boxA = Bodies.rectangle(400, 200, 80, 80);
+        var boxB = Bodies.rectangle(450, 50, 80, 80);
+        var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+        // add all of the bodies to the world
+        Composite.add(engine.world, [boxA, boxB, ground]);
+
+        // run the renderer
+        Render.run(render);
+
+        // create runner
+        var runner = Runner.create();
+
+        // run the engine
+        Runner.run(runner, engine);
+    }, []);
+
     return <>Hello World</>;
 }
 
